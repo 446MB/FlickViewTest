181de40b1e05fc529a66ce5193cb8522e029b4db
446MB <ichi072696@gmail.com>
2013-03-02 03:26:12 +0900

    firstCommit

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickViewTest
+//
+//  Created by Kenichi Yoshimura on 2013/03/02.
+//  Copyright (c) 2013年 Yoshimura Kenichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickViewTest
+//
+//  Created by Kenichi Yoshimura on 2013/03/02.
+//  Copyright (c) 2013年 Yoshimura Kenichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
